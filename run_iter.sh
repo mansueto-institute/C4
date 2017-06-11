6d72e14edc7466e1ce6bd584e307f5b4d92cc06f
Jamie Saxon <james.saxon@gmail.com>
2017-06-10 22:17:15 -0500

    reinstating everything

--- run_iter.sh
+++ run_iter.sh
@@ -10,25 +10,25 @@ shading="none"
 
 if [[ $x -eq 0 ]]; then ./run.py -s ${s} -i split -l0 --print_init -w ${s}/split/s000 --shading target district; fi
 
-for c in $(seq 2); do
+for c in $(seq 20); do
   ./run.py -s ${s} -i power:100000 -t 0.01 -x${x} -l0 --print_init -w ${s}/power/$(printf "s%03d" $x)/$(printf "c%03d" $c) --shading $shading
 done
 
 ./run.py -s ${s} -m dist_a       -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 1000 --destrand_min 5 --destrand_max 50 --tabu_length 2   --shading $shading 
-# ./run.py -s ${s} -m dist_p       -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 1000 --destrand_min 5 --destrand_max 50 --tabu_length 2   --shading $shading 
-# ./run.py -s ${s} -m dyn_radius   -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 5 --destrand_max 50                   --shading $shading 
-# ./run.py -s ${s} -m harm_radius  -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 5 --destrand_max 50 --allow_trades    --shading $shading 
-# ./run.py -s ${s} -m mean_radius  -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 5 --destrand_max 50                   --shading $shading 
-# ./run.py -s ${s} -m polsby       -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 1000 --destrand_min 5 --destrand_max 50 --tabu_length 2   --shading $shading    
-# ./run.py -s ${s} -m rohrbach     -t 0.02 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 5 --destrand_max 50                   --shading $shading      
-# ./run.py -s ${s} -m reock        -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 5 --destrand_max 50 --allow_trades    --shading $shading  -o scc -p scc
-# ./run.py -s ${s} -m ehrenburg    -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 3 --destrand_max 50 --allow_trades    --shading $shading  -o lic -p lic -r
-# ./run.py -s ${s} -m hull_a       -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 5 --destrand_max 50 --allow_trades    --shading $shading  
-# ./run.py -s ${s} -m hull_p       -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 3 --destrand_max 50 --allow_trades    --shading $shading  
-# ./run.py -s ${s} -m exchange     -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 3 --destrand_max 50                   --shading $shading  
-# ./run.py -s ${s} -m inertia_a    -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 5 --destrand_max 50                   --shading $shading 
-# ./run.py -s ${s} -m inertia_p    -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 5 --destrand_max 50                   --shading $shading 
-# ./run.py -s ${s} -m axis_ratio   -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 5 --destrand_max 50                   --shading $shading 
+./run.py -s ${s} -m dist_p       -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 1000 --destrand_min 5 --destrand_max 50 --tabu_length 2   --shading $shading 
+./run.py -s ${s} -m dyn_radius   -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 5 --destrand_max 50                   --shading $shading 
+./run.py -s ${s} -m harm_radius  -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 5 --destrand_max 50 --allow_trades    --shading $shading 
+./run.py -s ${s} -m mean_radius  -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 5 --destrand_max 50                   --shading $shading 
+./run.py -s ${s} -m polsby       -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 1000 --destrand_min 5 --destrand_max 50 --tabu_length 2   --shading $shading    
+./run.py -s ${s} -m rohrbach     -t 0.02 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 5 --destrand_max 50                   --shading $shading      
+./run.py -s ${s} -m reock        -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 5 --destrand_max 50 --allow_trades    --shading $shading  -o scc -p scc
+./run.py -s ${s} -m ehrenburg    -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 3 --destrand_max 50 --allow_trades    --shading $shading  -o lic -p lic -r
+./run.py -s ${s} -m hull_a       -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 5 --destrand_max 50 --allow_trades    --shading $shading  
+./run.py -s ${s} -m hull_p       -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 3 --destrand_max 50 --allow_trades    --shading $shading  
+./run.py -s ${s} -m exchange     -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 3 --destrand_max 50                   --shading $shading  
+./run.py -s ${s} -m inertia_a    -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 5 --destrand_max 50                   --shading $shading 
+./run.py -s ${s} -m inertia_p    -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 5 --destrand_max 50                   --shading $shading 
+./run.py -s ${s} -m axis_ratio   -t 0.01 -x${x} -n$max_iter -c $cycles --conv_iter 500  --destrand_min 5 --destrand_max 50                   --shading $shading 
 
 # if [[ $x -le 1 ]]; then ./run.py -s ${s} -m path_frac -t 0.05 -n2500 -x${x} --destrand_min 20 --destrand_max 5000 --shading target district; fi
 
