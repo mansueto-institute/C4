bea4dce56a11b35c83213d9bf7f9e6975c717b84
Jamie Saxon <jsaxon@uchicago.edu>
2017-12-15 14:44:34 -0600

    fewer iterations for path frac

--- run_iter.sh
+++ run_iter.sh
@@ -16,19 +16,20 @@ shading="none"
 if [[ "$METHOD" == "SPLIT" ]]; then 
   ./run.py -s ${s} -i split -l0 --print_init -w ${s}/split/s001 --shading $shading
 elif [[ "$METHOD" == "PATH_FRAC" ]]; then
-  ./run.py -s ${s} -m path_frac    -t 0.01 -n$N -c$C --conv_iter 100 -x${x} --destrand_min 20 --destrand_max 50 --shading $shading -v 1 --tabu_length 3
+  ./run.py -s ${s} -m path_frac    -t 0.01  -x${x} -n$N -c5  --conv_iter 100  --destrand_min 20 --destrand_max 50 --shading $shading -v 1 --tabu_length 3
 elif [[ "$METHOD" == "DIST" ]]; then
   ./run.py -s ${s} -m dist_a       -t 0.01  -x${x} -n$N -c$C --conv_iter 1000 --destrand_min 5 --destrand_max 50 --tabu_length 2                  --shading $shading 
   ./run.py -s ${s} -m dist_p       -t 0.01  -x${x} -n$N -c$C --conv_iter 1000 --destrand_min 5 --destrand_max 50 --tabu_length 2                  --shading $shading 
 elif [[ "$METHOD" == "RADII" ]]; then
-  ./run.py -s ${s} -m rohrbach     -t 0.005 -x${x} -n$N -c$C --conv_iter 1000  --destrand_min 5 --destrand_max 50                                  --shading $shading      
-  ./run.py -s ${s} -m harm_radius  -t 0.005 -x${x} -n$N -c$C --conv_iter 1000  --destrand_min 5 --destrand_max 50 --allow_trades                   --shading $shading 
-  ./run.py -s ${s} -m mean_radius  -t 0.005 -x${x} -n$N -c$C --conv_iter 1000  --destrand_min 5 --destrand_max 50                                  --shading $shading 
-  ./run.py -s ${s} -m dyn_radius   -t 0.005 -x${x} -n$N -c$C --conv_iter 1000  --destrand_min 5 --destrand_max 50                                  --shading $shading 
+  ./run.py -s ${s} -m rohrbach     -t 0.005 -x${x} -n$N -c$C --conv_iter 1000 --destrand_min 5 --destrand_max 50                                  --shading $shading      
+  ./run.py -s ${s} -m harm_radius  -t 0.005 -x${x} -n$N -c$C --conv_iter 1000 --destrand_min 5 --destrand_max 50 --allow_trades                   --shading $shading 
+  ./run.py -s ${s} -m mean_radius  -t 0.005 -x${x} -n$N -c$C --conv_iter 1000 --destrand_min 5 --destrand_max 50                                  --shading $shading 
+  ./run.py -s ${s} -m dyn_radius   -t 0.005 -x${x} -n$N -c$C --conv_iter 1000 --destrand_min 5 --destrand_max 50                                  --shading $shading 
 elif [[ "$METHOD" == "IPQ" ]]; then
   ./run.py -s ${s} -m polsby       -t 0.01  -x${x} -n$N -c$C --conv_iter 1000 --destrand_min 5 --destrand_max 50 --tabu_length 10 --allow_trades  --shading $shading 
 elif [[ "$METHOD" == "CIRCLES" ]]; then
-  ./run.py -s ${s} -m ehrenburg    -t 0.005 --ctol 0.02 -x${x} -n$N -c$C --conv_iter 500 --destrand_min 10 --destrand_max 50 --allow_trades --shading all -o lic -p lic -r -v 1
+  ./run.py -s ${s} -m ehrenburg    -t 0.005 -x${x} -n$N -c$C --conv_iter 500  --ctol 0.02 \
+                                                                              --destrand_min 10 --destrand_max 50 --allow_trades --shading $shading -o lic -p lic -r -v 1
   ./run.py -s ${s} -m reock        -t 0.01  -x${x} -n$N -c$C --conv_iter 500  --destrand_min 5 --destrand_max 50 --allow_trades                   --shading $shading  -o scc -p scc
   ./run.py -s ${s} -m exchange     -t 0.005 -x${x} -n$N -c$C --conv_iter 500  --destrand_min 5 --destrand_max 50                                  --shading $shading  
 elif [[ "$METHOD" == "HULL" ]]; then
