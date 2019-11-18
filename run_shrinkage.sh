8645ebb2215f00a32446f03de3445062b5058f78
Jamie Saxon <jsaxon@uchicago.edu>
2019-11-18 09:45:56 -0600

    shrinkage directory structure

--- run_shrinkage.sh
+++ run_shrinkage.sh
@@ -15,16 +15,16 @@ scaling="${SCALING:=1.0}"
 
 
 if [[ "$METHOD" == "POWER" ]]; then 
-  ./run.py -s ${s} -i power:100000 -t 0.01 -x ${x} -l0  -c100 --power_restart -w ${s}/power_regimes/s${x}/    --scale_regimes           $scaling --shading $shading --print_init -m power -v 1
+  ./run.py -s ${s} -i power:100000 -t 0.01 -x ${x} -l0  -c100 --power_restart -w ${s}/power_regimes/$scaling/s${x}/    --scale_regimes           $scaling --shading $shading --print_init -m power -v 1
 fi
 
 
 if [[ "$METHOD" == "HULL" ]]; then 
-  ./run.py -s ${s} -m hull_a       -t 0.01 -x ${x} -n$N -c$C  --conv_iter 250 -w ${s}/hull_a_regimes/s${x}/   --scale_regimes           $scaling --shading $shading --destrand_min 5 --destrand_max 50 --allow_trades -o hull -v 1
+  ./run.py -s ${s} -m hull_a       -t 0.01 -x ${x} -n$N -c$C  --conv_iter 250 -w ${s}/hull_a_regimes/$scaling/s${x}/   --scale_regimes           $scaling --shading $shading --destrand_min 5 --destrand_max 50 --allow_trades -o hull -v 1
 fi
 
 if [[ "$METHOD" == "IPQ" ]]; then 
-  ./run.py -s ${s} -m polsby_w     -t 0.01 -x ${x} -n$N -c$C  --conv_iter 500 -w ${s}/polsby_w_regimes/s${x}/ --scale_regime_perimeters $scaling --shading $shading --destrand_min 5 --destrand_max 50 --tabu_length 10 --allow_trades --ctol 0.02 -v 1
+  ./run.py -s ${s} -m polsby_w     -t 0.01 -x ${x} -n$N -c$C  --conv_iter 500 -w ${s}/polsby_w_regimes/$scaling/s${x}/ --scale_regime_perimeters $scaling --shading $shading --destrand_min 5 --destrand_max 50 --tabu_length 10 --allow_trades --ctol 0.02 -v 1
 fi
 
 
